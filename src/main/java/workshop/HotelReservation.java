a874d62723cb60eff9eaf48dc546b9a3367453f2
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-03-06 09:52:06 +0530

    [Sankar]ADDED:First Commit

---
+++ src/main/java/workshop/HotelReservation.java
@@ -0,0 +1,10 @@
+package workshop;
+
+import java.util.ArrayList;
+
+
+public class HotelReservation {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }
+}
---
+++ src/test/java/workshop/HotelReservationTest.java
@@ -0,0 +1,10 @@
+package workshop;
+
+import org.junit.jupiter.api.Test;
+
+public class HotelReservationTest {
+    @Test
+    public void AbilityToAddHotel() {
+        HotelReservation hotel = new HotelReservation();
+    }
+}
